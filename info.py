be8e82894db180ebb490019a4258947f3a1dc981
TG Dragon <110241656+TG-Dragon@users.noreply.github.com>
2022-08-11 11:48:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ ADMINS.append(1684438752)
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
